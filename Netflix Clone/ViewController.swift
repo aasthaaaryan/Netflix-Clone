697301447ac14a9a851a96f7cbca8bbe7909fedd
aasthaaaryan16 <142575112+aasthaaaryan16@users.noreply.github.com>
2024-11-01 00:39:15 +0530

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Aastha Aaryan on 01/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
